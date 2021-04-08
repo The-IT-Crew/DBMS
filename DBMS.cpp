2d6396f14713ae9247195121fc3ea475c953dc6b
suvasish114 <misuvasish114@gmail.com>
2021-04-09 00:24:30 +0530

    structure updated

--- DBMS.cpp
+++ DBMS.cpp
@@ -1,47 +1,61 @@
-#include<iostream>
-#include<cstdlib>
+#include <iostream>
+#include <cstdlib>
+#include <map>
+#include <string>
 
 using namespace std;
 
 struct node{
-	// dynamic data 
-	int data;
+	map<int, int> data1;
 	struct node *link;
-}
-
-struct node* getnode();
+};
+struct node *getnode();
 struct node *create();
 
 /*----------driving code----------*/
 int main(){
-	// driving code
+	struct node *l1, *ptr;
+	l1 = create();
+	ptr = l1;
+	map<int, int> ::iterator itr; 
+	while(ptr!= NULL){
+		itr = ptr->data1.begin();
+		cout<<(*itr).first;
+		ptr = ptr->link;
+	}
 	return 0;
 }
 
 /*----------functions-----------*/
-struct node* getnode(){
-	struct node *temp=(struct node*)malloc(sizeof(struct node));
-	cin>>temp->data;
+struct node *getnode(){
+	int a;
+	int b;
+	struct node *temp = (struct node *)malloc(sizeof(struct node));
+	cout << "Enter data1: ";
+	cin>>a>>b;
+	// cin >> temp->data1;
+	temp->data1.insert({a,b});
 	return temp;
 }
 struct node *create(){
-	 struct node *head,*ptr,*temp;
-	 int choice;
-	 head=NULL;
-	 do
-	 {
-		temp=getnode();
-		if(head==NULL){
-			head=temp;
-			ptr=temp;
+	struct node *head, *ptr, *temp;
+	int choice;
+	head = NULL;
+	while (1){
+		temp = getnode();
+		if (head == NULL){
+			head = temp;
+			ptr = temp;
 		}
 		else{
-			ptr->link=temp;
-			ptr=ptr->link;
+			ptr->link = temp;
+			ptr = ptr->link;
 		}
-		cout<<"Enter choice(0/1) "
-		cin>>choice;
-	 }while(choice==1);
-	 ptr->link=NULL;
-	 return head;
+		cout << "Enter choice(0/1) ";
+		cin >> choice;
+		if (choice == 0)
+			break;
+	}
+	ptr->link = NULL;
+	return head;
 }
